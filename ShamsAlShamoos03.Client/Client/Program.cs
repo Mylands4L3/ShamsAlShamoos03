b449b95770e220a545514178f218bc3904935170
Mylands4L3 <ghitherly@yahoo.com>
2026-02-26 16:07:01 +0330

    اعمال تغییرات بهینه 140412071606

--- ShamsAlShamoos03.Client/Client/Program.cs
+++ ShamsAlShamoos03.Client/Client/Program.cs
@@ -26,8 +26,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // HttpClient
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
